d0d6f60d1ce6309bc841d0c34b547fa9a6f1bd1e
Adrián García <adriangargar99@gmail.com>
2024-04-05 17:16:58 +0200

    feat: move logic to SignUpForm

---
+++ ts-react/src/components/SignUpForm.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import { InputField } from "../components/InputField.js";
+import { Title } from "../components/Title";
+import { Button } from "../components/Button";
+import { translateError } from "../utils/translateError.js";
+import { useNavigate } from "react-router-dom";
+import { SignUpUseCase } from "../UseCases/SignUpUseCase";
+
+export const SignUpForm = () => {
+  const useCase = new SignUpUseCase();
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    setErrorMessage(null);
+  }, [email, password]);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    useCase
+      .execute({ email, password })
+      .then(() => {
+        navigate("/success");
+      })
+      .catch((error) => {
+        setErrorMessage(error.message);
+      });
+  };
+
+  return (
+    <form className="signup-form" onSubmit={handleSubmit}>
+      <Title>Sign up with email</Title>
+      <p>Enter your email address to create an account.</p>
+
+      <InputField
+        id="email"
+        labelText="Your email"
+        type="email"
+        value={email}
+        onChange={setEmail}
+      />
+      <InputField
+        id="password"
+        labelText="Your password"
+        type="password"
+        value={password}
+        onChange={setPassword}
+      />
+      {errorMessage && <p>{translateError(errorMessage)}</p>}
+      <Button title="Signup" />
+    </form>
+  );
+};
--- ts-react/src/pages/SignUp.tsx
+++ ts-react/src/pages/SignUp.tsx
@@ -1,60 +1,10 @@
-import { useEffect, useState } from "react";
 import "./SignUp.css";
-import { InputField } from "../components/InputField.js";
-import { Title } from "../components/Title";
-import { Button } from "../components/Button";
-import { translateError } from "../utils/translateError.js";
-import { useNavigate } from "react-router-dom";
-import { SignUpUseCase } from "../UseCases/SignUpUseCase";
+import { SignUpForm } from "../components/SignUpForm";
 
 export const SignUp = () => {
-  const useCase = new SignUpUseCase();
-  const navigate = useNavigate();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState(null);
-
-  useEffect(() => {
-    setErrorMessage(null);
-  }, [email, password]);
-
   return (
     <main className="signup-container">
-      <form
-        className="signup-form"
-        onSubmit={(event) => {
-          event.preventDefault();
-
-          useCase
-            .execute({ email, password })
-            .then(() => {
-              navigate("/success");
-            })
-            .catch((error) => {
-              setErrorMessage(error.message);
-            });
-        }}
-      >
-        <Title>Sign up with email</Title>
-        <p>Enter your email address to create an account.</p>
-
-        <InputField
-          id="email"
-          labelText="Your email"
-          type="email"
-          value={email}
-          onChange={setEmail}
-        />
-        <InputField
-          id="password"
-          labelText="Your password"
-          type="password"
-          value={password}
-          onChange={setPassword}
-        />
-        {errorMessage && <p>{translateError(errorMessage)}</p>}
-        <Button title="Signup" />
-      </form>
+      <SignUpForm />
     </main>
   );
 };
